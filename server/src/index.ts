13bf29b635c35b3ed4df623a4b651fefeb7043da
itxsoumya <itxsoumyaranjan@gmail.com>
2024-02-06 13:15:47 +0530

    backend Done

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,12 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import v1Router from './routes/index'
+import cors from 'cors'
 dotenv.config();
 
 const app: Express = express();
-const port = 8080;
-
+const port = process.env.PORT || 8080;
+app.use(cors)
 app.use(express.json());
 
 app.use('/api/v1', v1Router)
