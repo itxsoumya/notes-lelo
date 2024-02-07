06568ca6411a27678d61c3cf94a98a76c1b7fd4c
Soumya Ranjan <72642534+itxsoumya@users.noreply.github.com>
2024-02-07 22:23:24 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,12 +10,12 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
-app.use(cors());
-// app.use(cors({
+// app.use(cors());
+app.use(cors({
 
-//     origin: ['https://noteslelo.vercel.app', 'http://localhost:5173']
+    origin: ['https://noteslelo.vercel.app', 'http://localhost:5173']
 
-// }))
+}))
 app.use(express.json());
 // app.use(upload.any())
 
