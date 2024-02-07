b8826f9ec782dcc6b07749ddc8f2684f8e0fdc37
itxsoumya <itxsoumyaranjan@gmail.com>
2024-02-07 20:00:28 +0530

    fixed a lot of bug

--- client/urlMap.js
+++ client/urlMap.js
@@ -1,5 +1,13 @@
-export const userPingUrl = 'http://localhost:8080/api/v1/manager/me'
-export const managerAuthUrl = 'http://localhost:8080/api/v1/manager/auth'
-export const notesFilterUrl = 'http://localhost:8080/api/v1/notes/filter'
-export const notesAddUrl = 'http://localhost:8080/api/v1/notes/add'
-export const notesRecentUrl = 'http://localhost:8080/api/v1/notes/recent'
+// export const userPingUrl = 'http://localhost:8080/api/v1/manager/me'
+// export const managerAuthUrl = 'http://localhost:8080/api/v1/manager/auth'
+// export const notesFilterUrl = 'http://localhost:8080/api/v1/notes/filter'
+// export const notesAddUrl = 'http://localhost:8080/api/v1/notes/add'
+// export const notesRecentUrl = 'http://localhost:8080/api/v1/notes/recent'
+
+const BaseUrl = "https://noteslelo.up.railway.app/api/v1"
+
+export const userPingUrl = BaseUrl + '/manager/me'
+export const managerAuthUrl = BaseUrl + '/manager/auth'
+export const notesFilterUrl = BaseUrl + '/notes/filter'
+export const notesAddUrl = BaseUrl + '/notes/add'
+export const notesRecentUrl = BaseUrl + '/notes/recent'
